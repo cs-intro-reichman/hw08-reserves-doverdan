c8dfacb64b34294e93afc23d553905762b44b824
Dan Dover <45949873+doverdan@users.noreply.github.com>
2024-03-12 18:09:29 +0200

    CheckIfGood

--- Track.java
+++ Track.java
@@ -1,5 +1,7 @@
-/** Represents a music track. A track has a title (String), an artist (String), 
- *  and a duration (int), in seconds. */
+/**
+ * Represents a music track. A track has a title (String), an artist (String),
+ * and a duration (int), in seconds.
+ */
 class Track {
     private String title;
     private String artist;
@@ -12,8 +14,10 @@ class Track {
         this.duration = duration;
     }
 
-    /** Returns this track's data as "artist, title, minutes:seconds".
-     *  For example, "John Lennon, Imagine, 3:07" */
+    /**
+     * Returns this track's data as "artist, title, minutes:seconds".
+     * For example, "John Lennon, Imagine, 3:07"
+     */
     public String toString() {
         //// Replace the following statement with code that returns
         //// the data of this track according to the method's documentation.
@@ -24,17 +28,21 @@ class Track {
     public String getTitle() {
         return title;
     }
+
     /** Returns this track's artist. */
     public String getArtist() {
         return artist;
     }
+
     /** Returns this track's duration. */
     public int getDuration() {
         return duration;
     }
 
-    /** If this track's duration is shorter than the other track's duration
-     *  returns true; otherwise returns false. */
+    /**
+     * If this track's duration is shorter than the other track's duration
+     * returns true; otherwise returns false.
+     */
     public boolean isShorterThan(Track other) {
         return duration < other.duration;
     }
@@ -42,7 +50,8 @@ class Track {
     // Returns a string that represents the totalSeconds as "minutes:seconds",
     // Where seconds is always two digits. For example, "3:17" or "12:05".
     private String formattedDuration(int totalSeconds) {
-        //// replace the following statement with your code
-        return "";
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+        return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
     }
 }
